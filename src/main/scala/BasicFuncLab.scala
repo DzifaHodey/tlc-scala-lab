ae377213db9fd150fb507ed5816bced282031112
Dzifa Hodey <dzifahodey@gmail.com>
2021-11-11 18:06:10 +0000

    inheritance, traits, functional,collections

--- src/main/scala/BasicFuncLab.scala
+++ src/main/scala/BasicFuncLab.scala
@@ -3,13 +3,18 @@ object BasicFuncLab extends App {
   def operation(s: String): (Int, Int) => Int = {
 
     s match {
-      case "add" => (a: Int, b: Int) => (a + b)
-      case "subtract" => (a: Int, b: Int) => (a - b)
+      case "add" => (a: Int, b: Int) => a + b
+      case "subtract" => (a: Int, b: Int) => a - b
       case "power" => (a: Int, b: Int) => Math.pow(a, b).toInt
     }
   }
 
-  println(operation("power") (2, 2))
+  val add = operation("add")
+  val subtract = operation("subtract")
+  val power = operation("power")
+  println(power(2,3))
+
+//  println(operation("power") (2, 2))
 
 
 }
---
+++ src/main/scala/CollectionsLab.scala
@@ -0,0 +1,35 @@
+import java.util
+import scala.:+
+import scala.collection.{LinearSeq, SortedSet}
+import scala.util.Random
+
+object CollectionsLab {
+  def main(args: Array[String]): Unit = {
+//    println(remove(Seq(2,3,4,5,6), 3))
+//    println(removeAgain(Seq(34,6,7,8), 0))
+
+    println(lottery)
+  }
+
+  // remove element at given index from Sequence Collections
+  def remove(collection: Seq[Any], idx: Int) ={
+    var newCollection: Seq[Any] = Nil
+    for (i <- 0 to collection.length-1) {
+      if (i != idx) newCollection = newCollection:+collection(i)
+    }
+    newCollection
+  }
+
+  def removeAgain(collection: Seq[Any], idx: Int)={
+    collection.take(idx) ++ collection.drop(idx + 1)
+  }
+
+  // Lottery
+  def lottery ={
+    var numbers:SortedSet[Int] = SortedSet()
+    for (i <- 1 to 6){
+      numbers = numbers.+(Random.between(1,49))
+    }
+    numbers
+  }
+}
